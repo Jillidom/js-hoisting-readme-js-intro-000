97c9aaecbc1c5c5291bd55f8adf0a8b9f87b6902
Jillian Domingue <jmdomingue@gmail.com>
2020-02-02 20:10:30 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,12 @@
 var callMe = function callMe(){
-  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe";
   return lyric;
 }
-
+callMe();
 
 function crazy(){
 var crazy = function() {
